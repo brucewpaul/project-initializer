304173174f8c60dbf311f0f9def85afc8143739e
Savaughn Jones <savaughnjones@gmail.com>
2016-12-27 10:34:19 -0800

    Add basic react add setup with static html from server.

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+	entry: './client/react/index.jsx',
+	output: {
+		path: path.join(__dirname, 'client/dist'),
+		filename: 'bundle.js',
+	},
+	module: {
+		loaders:[
+			{
+				test: /.jsx?$/,
+				loader: 'babel-loader',
+				excludes: /node_modules/,
+				query: {
+					presets: ['es2015', 'react']
+				}
+			}
+		]
+	}
+};
