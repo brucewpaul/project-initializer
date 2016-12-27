60e4caa40a4d475ec018b345af1847f55eb04ad5
Bruce Paul <brucewpaul@gmail.com>
2016-12-27 13:31:10 -0800

    begin to comment code for end user

--- ingredients/ang-app/about/about.js
+++ ingredients/ang-app/about/about.js
@@ -1,7 +1,12 @@
 'use strict';
 
+// we create a module on our app, and we will require ngRoute, allowing us to be displayed when on the current route
+// More Info: https://docs.angularjs.org/api/ngRoute
 angular.module('myApp.about', ['ngRoute'])
 
+// We only want to display this view if we're on the home ('/') route
+  // This works because we included ngRoute above
+// More Info: https://docs.angularjs.org/api/ngRoute/provider/$routeProvider
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/about', {
     templateUrl: 'about/about.html',
@@ -9,6 +14,8 @@ angular.module('myApp.about', ['ngRoute'])
   });
 }])
 
-.controller('AboutController', [function($scope, Items) {
+// We create a HomeController and assign it to this route. This allows us to specific things with this view (i.e. get data from the server and add it to the scope where we can then display it)
+// More Info ($scope): https://docs.angularjs.org/guide/scope
+.controller('AboutController', ['$scope', function($scope) {
 
 }]);
--- ingredients/ang-app/app.js
+++ ingredients/ang-app/app.js
@@ -7,7 +7,9 @@ angular.module('myApp', [
   'myApp.about'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+  // We are going to add to add a ! to the url
   $locationProvider.hashPrefix('!');
 
+  // if no route is found, send user to '/' (home)
   $routeProvider.otherwise({redirectTo: '/'});
 }]);
