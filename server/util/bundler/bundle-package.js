3466d0755927e70af57240917494bf1f462c4ab6
Bruce Paul <brucewpaul@gmail.com>
2016-12-28 15:53:00 -0800

    (bundle) cleaner solution, optimized for adding additional options

--- server/util/bundler/bundle-package.js
+++ server/util/bundler/bundle-package.js
@@ -1,23 +1,26 @@
 var package = require('../../../ingredients/package/package.js');
 var angular = require('../../../ingredients/package/ang-dependencies.js');
+var react = require('../../../ingredients/package/react-dependencies.js');
 var sqlite = require('../../../ingredients/package/sqlite-dependencies.js');
 
 module.exports = function(options) {
   if ( options.frontEnd.framework === 'Angular' ) {
-    package.scripts = angular.scripts;
-    package.dependencies = angular.dependencies;
-    package.devDependencies = angular.devDependencies;
-  }
-
-  if ( options.frontEnd.framework === 'React' ) {
+    frontEndFramework = angular;
+  } else if ( options.frontEnd.framework === 'React' ) {
+    frontEndFramework = react;
   }
 
   if ( options.backEnd.database === 'Sqlite3' ) {
-    package.dependencies = Object.assign(package.dependencies, sqlite.dependencies);
+    backEndDatabase = sqlite;
+  } else if ( options.frontEnd.framework === 'Mongo' ) {
+    backEndDatabase = Mongo;
   }
 
-  if ( options.backEnd.database === 'Mongo' ) {
-  }
+  package.scripts = frontEndFramework.scripts;
+  package.dependencies = frontEndFramework.dependencies;
+  package.devDependencies = frontEndFramework.devDependencies;
+
+  package.dependencies = Object.assign(package.dependencies, backEndDatabase.dependencies);
 
   return package;
 }
