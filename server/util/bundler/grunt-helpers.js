5b7d6e0923f70b70cece1f699dc37ce1d82dbdbd
Vinh Vu <vu.vinh.van@gmail.com>
2016-12-29 14:29:37 -0800

    Complete Gruntfile module

---
+++ server/util/bundler/grunt-helpers.js
@@ -0,0 +1,126 @@
+var fs = require('fs');
+var _ = require('lodash');
+
+var options = {
+  frontEnd: {
+    framework: 'react',
+    css: null,
+    taskRunner: {
+      type: 'grunt',
+      plugins: ['cssmin', 'uglify'],
+      tasks: [
+        {
+          name: 'cssmin',
+          plugins: ['cssmin']
+        },
+        {
+          name: 'uglify',
+          plugins: ['uglify']
+        },
+        {
+          name: 'build',
+          plugins: ['cssmin', 'uglify']
+        }
+      ]
+    },
+    testing: null
+  },
+  backend: {
+    database: null
+  }
+}
+
+var gruntMain = [
+  'module.exports = function(grunt) {\n',
+  '}\n'
+];
+
+var gruntInitConfig = [
+  '  grunt.initConfig({\n',
+  '    pkg: grunt.file.readJSON(\'package.json\')\n',
+  '  });\n'
+]
+
+var pluginConfigs = {
+  cssmin: [
+    '    cssmin: {\n',
+    '      target: {\n',
+    '        files: [{\n',
+    '          expand: true,\n',
+    {react: '          cwd: \'public/assets\',\n',
+    angular: '          cwd: \'assets\',\n'},
+    '          src: [\'*.css\', \'!*.min.css\'],\n',
+    '          dest: \'release/css\',\n',
+    '          ext: \'.min.css\'\n',
+    '        }]\n',
+    '      }\n',
+    '    }\n'
+  ],
+  uglify: [
+    '    uglify: {\n',
+    '      target: {\n',
+    '        files: {\n',
+    {react: '          \'public/assets/script.min.js\': [\'public/assets/script.js\']\n',
+    angular: '          \'assets/script.min.js\': [\'assets/script.js\']\n'},
+    '        }\n',
+    '      }\n',
+    '    }\n',
+  ]
+}
+
+var loadNpmTasks = {
+  cssmin: '  grunt.loadNpmTasks(\'grunt-contrib-cssmin\');\n',
+  uglify: '  grunt.loadNpmTasks(\'grunt-contrib-uglify\');\n'
+}
+// grunt.registerTask('default', ['jshint']);
+var createGruntTask = function(task) {
+  var gruntTask = '  grunt.registerTask(';
+  gruntTask = gruntTask + "'" + task.name + "', [";
+  _.forEach(task.plugins, function(plugin, index) {
+    var formattedPlugin = "'" + plugin + "'";
+    if (index !== task.plugins.length - 1) {
+      gruntTask = gruntTask + formattedPlugin + ', ';
+    } else {
+      gruntTask += formattedPlugin;
+    }
+  });
+  gruntTask += ']);\n';
+  return gruntTask;
+}
+
+var createGruntfileContents = function(options) {
+  var gruntFile = '';
+  gruntFile += gruntMain[0];
+  gruntFile += gruntInitConfig[0];
+  gruntFile += gruntInitConfig[1];
+  // for each plugin, insert it into the config init
+  _.forEach(options.frontEnd.taskRunner.plugins, function(plugin) {
+    _.forEach(pluginConfigs[plugin], function(line) {
+      if (typeof line === 'object') {
+        gruntFile += line[options.frontEnd.framework];
+      } else {
+        gruntFile += line;
+      }
+    });
+  });
+  gruntFile += gruntInitConfig[2];
+  // for each plugin, load npm module
+  _.forEach(options.frontEnd.taskRunner.plugins, function(plugin) {
+    gruntFile += loadNpmTasks[plugin];
+  });
+  // for each task, create the task.
+  _.forEach(options.frontEnd.taskRunner.tasks, function(task) {
+    gruntFile += createGruntTask(task);
+  });
+  gruntFile += gruntMain[1];
+  return gruntFile;
+}
+
+var createGruntfile = function(options) {
+  fs.writeFile('Gruntfile.js', createGruntfileContents(options), function (err) {
+    if (err) throw err;
+    console.log("Gruntfile.js successfully created");
+  });
+}
+
+module.exports.createGruntfile = createGruntfile;
