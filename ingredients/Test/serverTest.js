083b81cf66e81aa4285ac9b2ac6a7570856d7a76
Bruce Paul <brucewpaul@gmail.com>
2016-12-29 13:53:30 -0800

    (test) incorporate test from server folder to new test folder

--- ingredients/Test/serverTest.js
+++ ingredients/Test/serverTest.js
@@ -1,7 +1,22 @@
 var request = require('request');
 var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../server/server.js');
 var should = chai.should();
 var expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe ('Server', function () {
+  it('should connect to localhost', function(done) {
+    chai.request(server)
+      .get('/')
+      .end(function(err, res) {
+        res.should.have.status(200);
+        done();
+      });
+  });
+});
 
 describe('API Routes', function() {
   it('should respond to GET requests to /api/items with a 200 status code', function(done) {
--- ingredients/package/mocha-dependencies.js
+++ ingredients/package/mocha-dependencies.js
@@ -8,5 +8,6 @@ exports.dependencies = {
 exports.devDependencies = {
   "chai": "^3.5.0",
   "mocha": "^3.2.0",
-  "request": "^2.70.0"
+  "request": "^2.70.0",
+  "chai-http": "^3.0.0"
 }
