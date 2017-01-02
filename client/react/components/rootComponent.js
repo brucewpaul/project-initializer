eaf8cb2819b4a1643e30478ff0bd4cfb83632ed0
Savaughn Jones <savaughnjones@gmail.com>
2017-01-02 11:19:45 -0800

    Add basic views for MVP.

--- client/react/components/rootComponent.js
+++ client/react/components/rootComponent.js
@@ -2,21 +2,21 @@ import React from 'react';
 import { Grid, Row, Col, Button, Jumbotron, PageHeader, Image } from 'react-bootstrap';
 
 class Root extends React.Component {
-	constructor(props) {
-		super(props)
-	}
+  constructor(props) {
+    super(props)
+  }
 
-	render() {
-		return (
-			<div>
-			  <PageHeader className='projectTitle'>
+  render() {
+    return (
+      <div>
+        <PageHeader className='projectTitle'>
           Project-Initializer<br/>
           <small>Your dream project is a click away</small>
         </PageHeader>
-				{this.props.children}
-			</div>
-		)
-	}
+        {this.props.children}
+      </div>
+    )
+  }
 }
 
 export default Root
--- client/react/reducers/reduceOptions.js
+++ client/react/reducers/reduceOptions.js
@@ -1,9 +1,35 @@
 const initialState = {
   frontEnd:{
-    framework: null
+    framework: null,
+    styling: 'Javascipt/html/css'
   },
   backEnd: {
     database: null
+  },
+  devtools: {
+    taskrunner: {
+      name: 'grunt',
+      plugins:['cssmin', 'uglify'],
+      tasks:[
+      {
+        name: 'cssmin',
+        plugins:['cssmin']
+      },
+      {
+        name:'uglify',
+        plugins:['uglify']
+      },
+      {
+        name: 'build',
+        plugins: ['cssmin', 'uglify']
+      }
+      ]
+    },
+    bundler:{
+      name: 'webpack',
+      config:[]
+    },
+    testing: 'mocha/chai'
   }
 }
 
@@ -11,16 +37,49 @@ export default function(state = initialState, action) {
 
   switch(action.type) {
     case 'FRAMEWORK_CHOSEN':
-    console.log(state);
       return Object.assign({}, state, {
         frontEnd: {
-          framework: action.payload
+          framework: action.payload,
+          styling: state.frontEnd.styling
+        }
+      })
+    case 'STYLING_CHOSEN':
+      return Object.assign({} , state, {
+        frontEnd: {
+          framework: state.frontEnd.framework,
+          styling: action.payload
         }
       })
     case 'DATABASE_CHOSEN':
       return Object.assign({} , state, {
         backEnd: {
           database: action.payload
+        }
+      })
+    case 'TASKRUNNER_CHOSEN':
+      return Object.assign({} , state, {
+        devtools: {
+          taskrunner: {
+            name: action.payload.name,
+            tasks:[]
+          }
+        }
+      })
+    case 'TASK_ADDED':
+      return Object.assign({} , state, {
+        devtools: {
+          taskrunner: {
+            name: state.devtools.taskrunner.name,
+            tasks:[
+            ...state.devtools.taskrunner.tasks,
+
+            {
+              name: action.payload.name,
+              plugins: action.payload.plugins
+            }
+
+            ]
+          }
         }
       })
     default:
