043c8815643f6e5ac7ffd00967fc449365b41eeb
Vinh Vu <vu.vinh.van@gmail.com>
2017-01-03 15:54:12 -0800

    Add grunt dependency ingredients to package.json ingredient

--- server/util/bundler/bundle-package.js
+++ server/util/bundler/bundle-package.js
@@ -1,11 +1,12 @@
 var path = require('path');
-
+var _ = require('lodash');
 var angular = require(path.join(__dirname, '../../../ingredients/package/ang-dependencies.js'));
 var mocha = require(path.join(__dirname, '../../../ingredients/package/mocha-dependencies.js'));
 var mongo = require(path.join(__dirname, '../../../ingredients/package/mongo-dependencies.js'));
 var package = require(path.join(__dirname, '../../../ingredients/package/package.js'));
 var react = require(path.join(__dirname, '../../../ingredients/package/react-dependencies.js'));
 var sqlite = require(path.join(__dirname, '../../../ingredients/package/sqlite-dependencies.js'));
+var grunt = require(path.join(__dirname, '../../../ingredients/package/grunt-dependencies.js'));
 
 module.exports = function(options) {
   var frontEndFramework, backEndDatabase;
@@ -33,6 +34,14 @@ module.exports = function(options) {
       devDependencies: {}
     }
   }
+  var taskRunnerDependencies;
+  if ( options && options.devTools.taskRunner.name === 'grunt' ) {
+    taskRunnerDependencies = Object.assign({}, grunt.mainDependencies);
+    _.forEach(options.devTools.taskRunner.plugins, function(plugin) {
+      taskRunnerDependencies = Object.assign(taskRunnerDependencies, grunt[plugin]);
+    });
+    package.devDependencies = Object.assign(taskRunnerDependencies);
+  }
 
   if ( options && options.devTools.testing === 'Mocha' ) {
     tesing = mocha;
@@ -47,7 +56,7 @@ module.exports = function(options) {
   // combine the different dependencies and scripts from front end and backend modules
   package.scripts = Object.assign(frontEndFramework.scripts, backEndDatabase.scripts, tesing.scripts);
   package.dependencies = Object.assign(frontEndFramework.dependencies, backEndDatabase.dependencies, tesing.dependencies);
-  package.devDependencies = Object.assign(frontEndFramework.devDependencies, backEndDatabase.devDependencies, tesing.devDependencies);
+  package.devDependencies = Object.assign(package.devDependencies, frontEndFramework.devDependencies, backEndDatabase.devDependencies, tesing.devDependencies);
 
   return package;
 }
