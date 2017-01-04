5d29c0d07fc4946c816781a2e6a5605c395dfdce
Vinh Vu <vu.vinh.van@gmail.com>
2017-01-04 12:51:15 -0800

    Refactor server for mongo

--- ingredients/Mongo/controllers/index.js
+++ ingredients/Mongo/controllers/index.js
@@ -1,42 +1,23 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
+var Item = require('../db/models/Item.js');
 
-var db = mongoose.connection;
-var Item;
-
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('We\'re connected to MongoDB');
-
-  var itemSchema = mongoose.Schema({
-    title: String,
-    text: String
-  });
-
-  Item = mongoose.model('Item', itemSchema);
-});//connect and say success or error
-
-module.exports = {
-  items: {
-    get: function(req, res) {
-      Item.find(function(err, items) {
-        if (err) {
-          return console.error(err);
-        }
-        res.status(200).send({results: items});
-      });
-    }, // a function which handles a get request for all messages
-    post: function(req, res) {
-      var itemToAdd = new Item({
-        title: req.body.title,
-        text: req.body.text
-      });
-      itemToAdd.save(function(err, itemToAdd) {
-        if (err) {
-          return console.error(err);
-        }
-        res.status(201).send(itemToAdd);
-      });
+module.exports.retreiveAll = function(req, res) {
+  Item.find(function(err, items) {
+    if (err) {
+      return console.error(err);
     }
-  }
-};
+    res.status(200).send({results: items});
+  });
+}
+
+module.exports.createOne = function(req, res) {
+  var itemToAdd = new Item({
+    title: req.body.title,
+    text: req.body.text
+  });
+  itemToAdd.save(function(err, itemToAdd) {
+    if (err) {
+      return console.error(err);
+    }
+    res.status(201).send(itemToAdd);
+  });
+}
