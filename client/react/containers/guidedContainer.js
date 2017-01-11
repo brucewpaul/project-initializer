2b8583ecf3f4f93b11d1f1ecef4ab4b34486b2d4
Savaughn Jones <savaughnjones@gmail.com>
2017-01-11 14:21:32 -0800

    Refractor cart component.

--- client/react/containers/guidedContainer.js
+++ client/react/containers/guidedContainer.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import GuidedCartContainer from './guidedCartContainer';
+import { Grid, Row, Col } from 'react-bootstrap';
 
 class GuidedContainer extends React.Component {
 
   render() {
     return (
       <div>
-          {this.props.children}
+        <GuidedCartContainer />
+        {this.props.children}
       </div>
     )
   }
