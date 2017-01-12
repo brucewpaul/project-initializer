709e8174730142294bc875bd3756f6cb3938e220
Vinh Vu <vu.vinh.van@gmail.com>
2017-01-12 10:56:12 -0800

    Implement basic react testing

--- ingredients/package/mocha-enzyme-dependencies.js
+++ ingredients/package/mocha-enzyme-dependencies.js
@@ -6,11 +6,12 @@ exports.dependencies = {
 }
 
 exports.devDependencies = {
-  "babel-register": "^6.18.0",
   "babel": "^6.5.2",
-  "react-addons-test-utils": "^15.4.2",
-  "mocha": "^3.2.0",
-  "enzyme": "^2.7.0",
+  "babel-register": "^6.18.0",
   "jsdom": "^9.9.1",
+  "mocha": "^3.2.0",
   "chai": "^3.5.0",
+  "chai-http": "^3.0.0",
+  "enzyme": "^2.7.0",
+  "react-addons-test-utils": "^15.4.2"
 }
